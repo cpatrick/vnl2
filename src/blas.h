d69de6a5a7caefa191118c198362b60253c2e1e6
Chuck Atkins <chuck.atkins@kitware.com>
2010-06-30 15:30:58 -0400

    Added extern stubs for sscal in BLAS.

---
+++ src/blas.h
@@ -0,0 +1,31 @@
+#ifndef VNL2_BLAS_H_
+#define VNL2_BLAS_H_
+
+/**
+ * \file blas.h
+ * \author Chuck Atkins
+ *
+ * \verbatim
+ *   This provides an interface to the fortran routines in BLAS
+ * \endverbatim
+ */
+
+#include <complex>
+
+extern "C"
+{
+
+// Scalar multiplication
+extern void sscal_(const int *n, const float *alpha, 
+                   float *x, const int *incx);
+extern void dscal_(const int *n, const double *alpha,
+                   double *x, const int *incx);
+extern void cscal_(const int *n, const std::complex<float> *alpha, 
+                   std::complex<float> *x, const int *incx);
+extern void zscal_(const int *n, const std::complex<double> *alpha, 
+                   std::complex<double> *x, const int *incx);
+  
+}
+
+#endif
+  
