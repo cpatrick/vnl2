d4fab82da49131f7d58180060ae2784f190a25b0
Chuck Atkins <chuck.atkins@kitware.com>
2010-07-01 10:53:54 -0400

    Added an empty stub for testing the vector type.

---
+++ tests/test_vnl2_vector.cxx
@@ -0,0 +1,15 @@
+#include <cstdlib>
+
+#include <iostream>
+#include <vector>
+
+#include <vnl2_types.h>
+#include <vnl2_vector.h>
+
+#include <testlib/testlib_test.h>
+
+int main(int argc, const char *argv[])
+{
+  return EXIT_FAILURE;
+}
+
